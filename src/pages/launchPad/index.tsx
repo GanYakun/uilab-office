59925836eb5e00fd4818ed8cc49ceb682521af16
lx.jin <308561217@qq.com>
2023-11-20 12:24:09 +0800

    routes update

---
+++ src/pages/launchPad/index.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: lx.jin 308561217@qq.com
+ * @Date: 2022-08-04 18:10:09
+ * @LastEditors: lx.jin 308561217@qq.com
+ * @LastEditTime: 2023-11-17 18:18:13
+ * @FilePath: /qiankun/uiLab/apps/launchPad/src/pages/LaunchPad.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from 'react';
+import './index.less';
+
+
+const LaunchPad: React.FC = () => {
+  return <div>
+    LaunchPad
+  </div>
+}
+
+export default LaunchPad;
