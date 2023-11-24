a74507127ec3bcfffd4f8a26d9704a97fc9a80f4
lx.jin <308561217@qq.com>
2023-11-24 09:51:30 +0800

    appConfig update

--- config/routes.ts
+++ config/routes.ts
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-17 17:18:59
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-11-21 15:30:40
+ * @LastEditTime: 2023-11-24 09:28:45
  * @FilePath: /Uilab-Application/config/routes.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -30,12 +30,18 @@ export default [
   {
     path: '/menu1',
     name: 'Apps',
-    icon: 'smile',
+    icon: 'smile', 
     routes: [
       {
         name: 'RolesList',
         path: '/menu1/role-manage/RolesList',
         component: '../../lib/Uilab-Comp/smart-comp/UIPages/ListReport',
+      },
+      {
+        name: 'RolesObjectPage',
+        path: '/menu1/role-manage/RolesObjectPage',
+        component: '../../lib/Uilab-Comp/smart-comp/UIPages/ObjectPage',
+        hideInMenu: true,
       },
     ],
   },
