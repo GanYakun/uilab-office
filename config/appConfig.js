e9c1745adec216cf105c22236a3476c8d048a6ba
lx.jin <308561217@qq.com>
2023-11-29 17:16:03 +0800

    appconfig update

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-23 10:51:23
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-11-29 15:51:10
+ * @LastEditTime: 2023-11-29 16:29:51
  * @FilePath: /Uilab-Application/config/appConfig.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,7 +12,7 @@
  */
 const appConfig = {
     //应用名称
-    title: 'Gconfig',
+    title: 'OfficeAuto',
     subTitle: '工至可配置业务平台系统v0.1',
     logo: '/try8.svg',
     //Fe应用
