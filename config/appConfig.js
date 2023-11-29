252426b20737fd8235999dbba5da24afe3b095a1
lx.jin <308561217@qq.com>
2023-11-29 15:54:02 +0800

    appconfig update

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-23 10:51:23
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-11-29 15:24:26
+ * @LastEditTime: 2023-11-29 15:51:10
  * @FilePath: /Uilab-Application/config/appConfig.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,8 +28,9 @@ const appConfig = {
             'supplier-dd-form'
         ]
     }],
+    //国际化配置
     locale: {
-        default: 'en-Us',
+        default: 'en-Us',//en-Us,zh-CN,zh-TW
         antd: true,
         baseNavigator: true,
     },
