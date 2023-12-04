62a0c22b94135c8949974600a1e4a94df61b18d9
lx.jin <308561217@qq.com>
2023-12-04 15:58:48 +0800

    add app:   'supplierparty-manage'

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-23 10:51:23
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-01 15:37:39
+ * @LastEditTime: 2023-12-04 15:55:21
  * @FilePath: /Uilab-Application/config/appConfig.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -30,6 +30,7 @@ const appConfig = {
             name: 'supplier',
             icon: 'smile',
             apps: [
+                'supplierparty-manage',
                 'supplierapprove-managebyapplication',
                 'supplierapprove-managebyprocurement',
                 'supplierapprove-managebycompliance',
