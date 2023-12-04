e51f6cc8dedf2b1c73d380a1e9acdf78530bde14
lx.jin <308561217@qq.com>
2023-12-04 14:21:45 +0800

    launchPad update

--- src/pages/launchPad/index.tsx
+++ src/pages/launchPad/index.tsx
@@ -2,14 +2,13 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-08-04 18:10:09
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-11-17 18:18:13
+ * @LastEditTime: 2023-12-04 14:21:22
  * @FilePath: /qiankun/uiLab/apps/launchPad/src/pages/LaunchPad.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useEffect } from 'react';
 import './index.less';
-import { getRouteFiles } from 'config/routes';
-
+import { appConfig } from '../../../config/appConfig'
 
 const LaunchPad: React.FC = () => {
   const data = [
@@ -44,7 +43,7 @@ const LaunchPad: React.FC = () => {
     }
   ]
   useEffect(() => {
-    // console.log(getRouteFiles());
+    //console.log({ appConfig })
   }, [])
   return <div>
     {data.map((item, index) => {
