882f479a0c109f3dfb6ab5f28001dda16ed130c6
lx.jin <308561217@qq.com>
2023-12-05 17:39:59 +0800

    build

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-28 09:05:26
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-11-30 15:24:21
+ * @LastEditTime: 2023-12-05 17:38:05
  * @FilePath: /Uilab-Application/config/config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,6 +17,9 @@ import { appConfig } from './appConfig';
 
 export default defineConfig({
   hash: true,
+  history: {
+    type: 'hash',
+  },
   antd: {
     //dark: true, //暗黑风格
     compact: true,
