5d58531f83a7635045dbf05407bd962431e897f0
lx.jin <308561217@qq.com>
2023-12-05 12:00:26 +0800

    routes icon update

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-23 10:51:23
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-05 10:37:13
+ * @LastEditTime: 2023-12-05 11:56:38
  * @FilePath: /Uilab-Application/config/appConfig.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,7 @@ const appConfig = {
         {
             path: 'menu1',
             name: 'user&permission',
-            icon: 'smile',
+            icon: 'ApartmentOutlined',
             apps: [
                 {
                     appName: 'role-manage',
@@ -31,7 +31,7 @@ const appConfig = {
         {
             path: 'menu2',
             name: 'supplier',
-            icon: 'smile',
+            icon: 'AppstoreOutlined',
             apps: [
                 {
                     appName: 'supplierparty-manage',
--- config/routes.ts
+++ config/routes.ts
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-17 17:18:59
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-05 10:39:14
+ * @LastEditTime: 2023-12-05 11:57:29
  * @FilePath: /Uilab-Application/config/routes.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -68,7 +68,7 @@ export default [
   {
     path: '/launchPad',
     name: 'launchPad',
-    icon: 'smile',
+    icon: 'InsertRowLeftOutlined',
     component: './launchPad',
   },
   ...getRouteFiles(),
