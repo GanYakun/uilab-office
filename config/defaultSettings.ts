fcaeb4562e18d31d678f3697eb2f55feeba92744
lx.jin <308561217@qq.com>
2023-12-06 12:29:30 +0800

    defaultSetting update

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2022-09-21 15:21:37
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-11-30 15:25:50
+ * @LastEditTime: 2023-12-06 12:21:08
  * @FilePath: /uilab-gbms/launchPad/config/defaultSettings.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,9 +13,9 @@ const Settings: LayoutSettings & {
   pwa?: boolean;
   logo?: string;
 } = {
-  "navTheme": 'dark',
-  "primaryColor": "#1890ff",
-  "layout": "side",
+  "navTheme": 'light',
+  "primaryColor": "#72b5f8",
+  "layout": "top",
   "contentWidth": "Fixed",
   "fixedHeader": true,
   "fixSiderbar": true,
