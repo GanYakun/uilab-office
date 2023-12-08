3d958db8dd12e9274ef1ee39f0a864c64609c549
lx.jin <308561217@qq.com>
2023-12-08 12:26:12 +0800

    build

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-23 10:51:23
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-07 15:39:36
+ * @LastEditTime: 2023-12-08 12:24:07
  * @FilePath: /Uilab-Application/config/appConfig.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -36,24 +36,6 @@ const appConfig = {
                 {
                     appName: 'supplierparty-manage',
                     access: 'can001',
-<<<<<<< Updated upstream
-                    "ListReport": [
-                        {
-                            children: "SmartTable",
-                            SmartProps: [
-                                {
-                                    data: {
-                                        
-                                    },
-                                    type: "columns",
-                                    index: 0,
-                                    operate: "add", // add, replace, delete
-                                }
-                            ]
-                        }
-                    ]
-=======
->>>>>>> Stashed changes
                 },
                 {
                     appName: 'supplierapprove-managebyapplication',
@@ -70,6 +52,21 @@ const appConfig = {
                                     },
                                     type: "HeaderFacets",
                                     operate: "add", // add, replace, delete
+                                }
+                            ]
+                        }
+                    ],
+                    "ListReport": [
+                        {
+                            children: "SmartTable",
+                            SmartProps: [
+                                {
+                                    data: {
+                                        width: 65
+                                    },
+                                    type: "columns",
+                                    index: 0,
+                                    operate: "addType", // add, replace, delete addType
                                 }
                             ]
                         }
@@ -100,7 +97,7 @@ const appConfig = {
                             SmartProps: [
                                 {
                                     data: {
-                                        width: 100
+                                        width: 65
                                     },
                                     type: "columns",
                                     index: 0,
