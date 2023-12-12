652a38ddd3fb422b3d3776b6ad9ee7c1c7f4e24a
lx.jin <308561217@qq.com>
2023-12-12 13:50:13 +0800

    appconfig update

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-23 10:51:23
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-08 14:14:49
+ * @LastEditTime: 2023-12-12 13:41:51
  * @FilePath: /Uilab-Application/config/appConfig.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,7 +18,7 @@ const appConfig = {
     //Fe应用
     feApps: [
         {
-            path: 'menu1',
+            path: 'user&permission',
             name: 'user&permission',
             icon: 'ApartmentOutlined',
             apps: [
@@ -29,7 +29,7 @@ const appConfig = {
             ],
         },
         {
-            path: 'menu2',
+            path: 'supplier',
             name: 'supplier',
             icon: 'AppstoreOutlined',
             apps: [
