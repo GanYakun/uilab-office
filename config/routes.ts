def7718a0b4c60f73c905acb1f15b4478eeb378d
lx.jin <308561217@qq.com>
2023-12-13 14:52:36 +0800

    routes  update

--- config/routes.ts
+++ config/routes.ts
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-17 17:18:59
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-05 18:16:46
+ * @LastEditTime: 2023-12-13 14:38:57
  * @FilePath: /Uilab-Application/config/routes.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,7 +14,7 @@ export default [
     path: '/launchPad',
     name: 'launchPad',
     icon: 'InsertRowLeftOutlined',
-    component: './launchPad',
+    component: '../../lib/Uilab-Comp/smart-comp/CommonPages/launchPad/index.tsx',
   },
   ...getRouteFiles(),
   {
@@ -24,10 +24,10 @@ export default [
       {
         name: 'login',
         path: '/user/login',
-        component: './user/Login',
+        component: '../../lib/Uilab-Comp/smart-comp/CommonPages/user/Login',
       },
       {
-        component: './404',
+        component: '../../lib/Uilab-Comp/smart-comp/CommonPages/404',
       },
     ],
   },
@@ -36,6 +36,6 @@ export default [
     redirect: '/launchPad',
   },
   {
-    component: './404',
+    component: '../../lib/Uilab-Comp/smart-comp/CommonPages/404',
   },
 ];
