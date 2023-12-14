b6349a4b1e8c64da651f320c20bd64a0e2d86a05
lx.jin <308561217@qq.com>
2023-12-14 12:12:36 +0800

    build

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-23 10:51:23
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-14 11:14:47
+ * @LastEditTime: 2023-12-14 12:09:25
  * @FilePath: /Uilab-Application/config/appConfig.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -26,6 +26,10 @@ const appConfig = {
                 {
                     appName: 'role-manage',
                     access: 'canAdmin',
+                },
+                {
+                    appName: 'uilab-app-manage',
+                    access: 'canAdmin',
                 }
             ],
         },
@@ -36,7 +40,7 @@ const appConfig = {
             apps: [
                 {
                     appName: 'supplierparty-manage',
-                    access: 'can001',
+                    access: 'can04',
                     "ListReport": [
                         {
                             children: "SmartTable",
@@ -141,10 +145,6 @@ const appConfig = {
                             ]
                         }
                     ]
-                },
-                {
-                    appName: 'supplier-dd-form',
-                    access: 'can04',
                 },
             ],
         }
