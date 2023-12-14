44de95300b7c63cdab25a77b810c5ba4bc2010e5
lx.jin <308561217@qq.com>
2023-12-14 08:35:30 +0800

    merge

--- config/appConfig.js
+++ config/appConfig.js
@@ -2,7 +2,7 @@
  * @Author: lx.jin 308561217@qq.com
  * @Date: 2023-11-23 10:51:23
  * @LastEditors: lx.jin 308561217@qq.com
- * @LastEditTime: 2023-12-12 13:41:51
+ * @LastEditTime: 2023-12-14 08:30:09
  * @FilePath: /Uilab-Application/config/appConfig.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,6 +15,7 @@ const appConfig = {
     title: 'OfficeAuto',
     subTitle: 'v0.1',
     logo: '/try8.svg',
+    copyright: 'DAYPOP TECHNOLOGY PROJECTS LLC',
     //Fe应用
     feApps: [
         {
